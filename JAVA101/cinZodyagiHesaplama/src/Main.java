dfa83db35d2f9c9ef9180e40bc9d71f72a22b777
Kerim Öztürk <kerimmozturk@hotmail.com>
2023-04-18 04:59:25 +0300

    PatikaDev Java101 kodları
    PatikaDev Java101 kodları

---
+++ JAVA101/cinZodyagiHesaplama/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner girdi = new Scanner(System.in);
+        System.out.println("Doğum yılınızı giriniz");
+        int dogumTarihi = girdi.nextInt();
+
+        int deger = dogumTarihi % 12;
+
+        switch (deger) {
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+
+        }
+    }
+}
