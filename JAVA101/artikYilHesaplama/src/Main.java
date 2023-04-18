08723ff2b70ef5c1dcd7a4962c1085ae2f2ed318
Kerim Öztürk <kerimmozturk@hotmail.com>
2023-04-18 06:14:16 +0300

    PatikaDev Java101 kodları
    PatikaDev Java101 kodları

--- JAVA101/artikYilHesaplama/src/Main.java
+++ JAVA101/artikYilHesaplama/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+System.out.println("Yıl değerini giriniz.");
+        Scanner input = new Scanner(System.in);
+        int yil=input.nextInt();
+
+        if (yil%100==0){
+            if (yil%400==0){
+                System.out.println(yil+" Artık yıldır.");
+            } else{
+                System.out.println(yil+" Artık yıl değildir.");
+            }
+        }
+        else if (yil%4==0){System.out.println(yil+" Artık yıldır.");}
+        else{
+            System.out.println(yil+" Artık yıl değildir.");
+        }
     }
 }
